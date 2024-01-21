680b8f040c8426abe078642b08f693f0549d4891
Vitaliy Buzovskiy <buzovskiy.v@gmail.com>
2024-01-21 23:50:43 +0100

    add webhook test

--- push/urls.py
+++ push/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import send_test
+from .views import send_test, webhook_test
 
 
 urlpatterns = [
-    path('send-test/', send_test)
+    path('send-test/', send_test),
+    path('webhook-test/', webhook_test),
 ]
--- push/views.py
+++ push/views.py
@@ -12,3 +12,11 @@ def send_test(request):
         push_data = request.data
         resp = _send_fcm_message(push_data)
         return Response(resp.text, status=resp.status_code)
+
+
+@api_view(['GET', 'POST'])
+def webhook_test(request):
+    if request.method == 'GET':
+        return Response('get: webhook_test')
+    if request.method == 'POST':
+        return Response('post: webhook_test', status=200)
